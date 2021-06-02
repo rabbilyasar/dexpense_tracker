c87ec6d6e3b5111e206be3157ff23d45702eba12
rabbilyasar <rabbilyasar@gmail.com>
2021-06-02 12:52:59 +0600

    heroku

--- dexpense_tracker/settings.py
+++ dexpense_tracker/settings.py
@@ -11,26 +11,26 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from sentry_sdk.integrations.django import DjangoIntegration
+# from sentry_sdk.integrations.django import DjangoIntegration
 import django_heroku
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
 
 
-sentry_sdk.init(
-    dsn="https://58f07ca486c84592bd0afbe1cb84cca3@o482045.ingest.sentry.io/5531602",
-    integrations=[DjangoIntegration()],
+# sentry_sdk.init(
+#     dsn="https://58f07ca486c84592bd0afbe1cb84cca3@o482045.ingest.sentry.io/5531602",
+#     integrations=[DjangoIntegration()],
 
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production,
-    traces_sample_rate=1.0,
+#     # Set traces_sample_rate to 1.0 to capture 100%
+#     # of transactions for performance monitoring.
+#     # We recommend adjusting this value in production,
+#     traces_sample_rate=1.0,
 
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
+#     # If you wish to associate users to errors (assuming you are using
+#     # django.contrib.auth) you may enable sending PII data.
+#     send_default_pii=True
+# )
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
